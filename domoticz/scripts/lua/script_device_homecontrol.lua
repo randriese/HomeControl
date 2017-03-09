3dd0663461df708b4aa83e9510c32db298cecff0
Roland Andriese <randriese@hotmail.com>
2017-03-09 20:05:02 +0000

    fixed lua script

--- domoticz/scripts/lua/script_device_homecontrol.lua
+++ domoticz/scripts/lua/script_device_homecontrol.lua
@@ -1,4 +1,5 @@
 cmd = ''
 for device,status in pairs(devicechanged) do cmd = cmd..device.."|"..status.."#" end
 os.execute('curl -s -d "c='..cmd..'" http://192.168.1.62/entry.php &')
-return {}
+commandArray = {}
+return commandArray
